35333145c7ead0bdbff077f4ceb99893c4613ceb
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2024-09-25 16:32:01 +0100

    Fixing date error validations

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function Header({onFetch}) {
     const [startDate, setStartDate] = React.useState('');
     const [endDate, setEndDate] = React.useState('');
-    const [dateError, setDateError] = React.useState(false);
-    const [dateError2, setDateError2] = React.useState(false);
+    const [dateError, setDateError] = React.useState('');
 
     const handleStartDate = (e) => {
         setStartDate(e.target.value);
@@ -13,17 +12,21 @@ export default function Header({onFetch}) {
         setEndDate(e.target.value);
     };
     const handleFetch = () => {
+        setDateError('');
+
         if(new Date(startDate) > new Date(endDate)) {
-            setDateError(true);
+            setDateError('Start date must be before end date');
             return;
         }
         if(new Date(endDate) > new Date()) {
-            setDateError2(true);
+            setDateError('End Date must be today or earlier');
+            return;
+        }
+        if (!startDate || !endDate) {
+            setDateError('Both dates must be selected');
             return;
         }
         onFetch(startDate, endDate);
-        setDateError(false);
-        setDateError2(false);
     };
 
     return (
@@ -34,8 +37,7 @@ export default function Header({onFetch}) {
                 <input className="end-date" type="date" value={endDate} onChange={handleEndDate}/>
                 <button className="btn-fetch-data" onClick={handleFetch}>Fetch Data</button>
             </div>
-            {dateError && (<p className="date-error">Start date must be before end date</p>)}
-            {dateError2 && (<p className="date-error">End Date must be today or earlier</p>)}            
+            {dateError && (<p className="date-error">{dateError}</p>)}        
         </div>
     )
 }
