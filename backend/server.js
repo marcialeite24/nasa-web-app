8bd2353afdad894e671aea18a2759a2fbe4d6441
marcialeite24 <38685980+marcialeite24@users.noreply.github.com>
2024-09-27 16:04:33 +0100

    Update vercel and server files

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,8 @@ app.get('/apod', async (req, res) => {
     try {
         const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}&start_date=${start_date}&end_date=${end_date}`);
         const data =  await response.json();
-        console.log(data);
         res.json(data);
     } catch (error) {
-        console.log(error.message);
         res.status(500).send('Error fetching data');
     }
 });
